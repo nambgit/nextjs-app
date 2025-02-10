45c95dd67f3d2a8f956760160490f1fc5003a7b2
Bui Giang Nam <buigiangnam@nambuis-MacBook-Air.local>
2025-02-10 09:07:58 +0700

    UPDATE

---
+++ build.sh
@@ -0,0 +1,29 @@
+END_POINT=$1
+TAG=$2
+VERSION=$(git rev-parse --verify HEAD)
+IMAGE_NAME=nambgit/lab-nextjs$TAG
+#IMAGE_NAME=ghcr.io/ascension-six/backmarket$TAG
+
+echo "Build docker image name " $IMAGE_NAME:$VERSION
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
+fi
+
+docker build -t $IMAGE_NAME:$VERSION . &&
+docker build -t $IMAGE_NAME:latest . &&
+docker push $IMAGE_NAME:$VERSION && 
+docker push $IMAGE_NAME:latest &&
+#echo $BMK_VERSION $END_POINT
+#curl --location --request POST 'https://cms-bmk.ascension6.dev/external_api/deploy/1bd8e53148f3d6ac42332c626d524bbb199a01bce67f7896e5da03dd7cc9d0e5' \
+#--header 'Content-Type: application/json' \
+#--header 'Cookie: device_view=full' \
+#--data-raw '{
+    #"version": {
+        #"name": "BMK_VERSION",
+        #"value": "'$VERSION'"
+    #},
+    #"stackId": '$END_POINT'
+#}'
+
+echo "Build success: $IMAGE_NAME:$VERSION"
