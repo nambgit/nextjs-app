1e646e8d13f1a81b3fbcef2968adac9f20eb38db
Bui Giang Nam <buigiangnam@nambuis-MacBook-Air.local>
2025-02-10 15:41:26 +0700

    v0.2.2

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@ END_POINT=$1
 TAG=$2
 VERSION=$(git rev-parse --verify HEAD)
 IMAGE_NAME=nambgit/lab-nextjs$TAG
-IMAGE_TAG=$(date +%Y%m%d%H%M%S)
 #IMAGE_NAME=ghcr.io/ascension-six/backmarket$TAG
 
 echo "Build docker image name " $IMAGE_NAME:$VERSION
@@ -13,8 +12,7 @@ fi
 
 docker build --no-cache -t $IMAGE_NAME:$VERSION . &&
 docker build --no-cache -t $IMAGE_NAME:latest . &&
-docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest &&
-docker push $IMAGE_NAME:$VERSION && 
+docker push $IMAGE_NAME:$VERSION &&
 docker push $IMAGE_NAME:latest &&
 #echo $BMK_VERSION $END_POINT
 #curl --location --request POST 'https://cms-bmk.ascension6.dev/external_api/deploy/1bd8e53148f3d6ac42332c626d524bbb199a01bce67f7896e5da03dd7cc9d0e5' \
