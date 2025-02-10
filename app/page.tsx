1b50cc1cac10701b65a7b897607805b85a5a0c99
Bui Giang Nam <buigiangnam@nambuis-MacBook-Air.local>
2025-02-10 16:32:01 +0700

    v0.2.7

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Version 0.2.6
+            Version 0.2.7
           </a>
         </div>
       </main>
