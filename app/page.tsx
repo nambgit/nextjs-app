f74b1cdb67e9d035d359758c16d1282b1e9b795e
Bui Giang Nam <buigiangnam@nambuis-MacBook-Air.local>
2025-02-11 15:47:37 +0700

    Scan Image

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-          Add Snyk to scan Vulneribilities
+          Add Scan Docker Image
           </a>
         </div>
       </main>
