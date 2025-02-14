8dadbcf8e7662ce45aa57b9135a2f036d94efdbb
Bui Giang Nam <buigiangnam@nambuis-MacBook-Air.local>
2025-02-14 15:33:59 +0700

    Build Push Deploy Rollback workflow with new soure07

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-          Update Rollback flow on GitHub Actions with new source 06
+          Update Rollback flow on GitHub Actions with new source 07
           </a>
         </div>
       </main>
